117b03c8ebd03f8d31955031cfd7bf908f3837e9
MukhammadSaidov <saidov.muhammad1402@gmail.com>
2022-01-20 16:29:36 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './components/Footer/footer'
+import AddTodo from './containers/AddTodo/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList/VisibleTodoList'
+
+export default function App() {
+    return (
+        <div className="app">
+            <h1>Todo App</h1>
+            <div id="divBlock">
+            <AddTodo />
+            <VisibleTodoList />
+            <Footer />
+            </div>
+        </div>
+    )
+
+}
